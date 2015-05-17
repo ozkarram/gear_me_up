a728e2aaf75a1c0a42b64fcaf19f0869cdb724d4
oscaralvarez <ozkar_ram@hotmail.com>
2015-05-16 19:36:08 -0500

    avance en muestra de datos

--- app/src/main/java/com/crashdev/gearmeup/MainActivity.java
+++ app/src/main/java/com/crashdev/gearmeup/MainActivity.java
@@ -22,18 +22,8 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void init() {
+        //init date picker
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
